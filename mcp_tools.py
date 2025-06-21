c960bfc1390e8155f4af82ab632af1bfe1c38a60
Eltaf-azizi <iltfhuss@gmail.com>
2025-06-21 11:34:38 +0500

    added all the dependencies

--- mcp_tools.py
+++ mcp_tools.py
@@ -4,10 +4,26 @@ from tools.symptom_extractor import extract_symptoms
 from tools.pubmed_fetcher import fetch_pubmed_articles_with_metadata
 from tools.summarizer import summarize_text
 
+mcp = FastMCP("Medical diagnosis custom mcp")
 
 
+
+@mcp.tool()
 def full_medical_analysis(symptom_text):
     symptoms = extract_symptoms(data.description)
     diagnosis = get_diagnosis(symptoms)
     pubmed_raw = fetch_pubmed_articles_with_metadata(" ".join(symptoms))
     summary = summarize_text(pubmed_raw[:3000])
+
+
+    return {
+        "symptom": symptoms,
+        "diagnosis": diagnosis,
+        "pubmed_summary": summary
+    }
+
+
+
+
+if __name__ == "__main__":
+    mcp.run()
